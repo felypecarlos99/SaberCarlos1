a4d8e18428c2cfbf9f04249958e842e26b75d697
felype carlos <felype.carlosilva@gmail.com>
2025-05-23 12:26:44 -0300

    salvar 23 05

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,47 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+let pessoas=[]
 function App() {
-  const [ count, setCount ] = useState(1) 
-  const [ texto, setTexto ] = useState("") 
+  const [ count, setNome ] = useState("") 
+  const [ texto, setIdade ] = useState("") 
 
   const handleClick = ()=> { 
-    setCount(count * 2) 
-    console.log(count)
+   pessoas.push({id:pessoas.length,nome, idade})
+    console.log(pessoas)
+    setNome("")
   }
 
-  const handleTexto = (e) => {
-    console.log(e.target.value)
-    setTexto(e.target.value)
+  const handleNome = (e) => {
+    setNome(e.target.value)
+  }
+
+  const handleIdade = (e) => {
+    setIdade(e.target.value)
   }
   return (
     <>
     <div className='container'>
     <div><h1>CHECKLIST</h1></div>
     <div className='card1'><input type='text' 
-                 placeholder='adicionar tarefa' 
-                  onChange={handleTexto}/>
-             <button onClick={handleClick}>Clique Aqui</button></div>
-    <div className='card2'></div>
+                 placeholder='digite aqui' 
+                  onChange={handleNome}
+                  value={nome}/>
+                  <input type="number"
+                  placeholder='digite aqui'
+                  onChange={handleIdade}
+                  value={idade}/>
+             <button onClick={handleClick}>Clique Aqui</button>
+             {pessoas.map(e=>(
+              <div key={e.nome}>
+                <p>Nome: {e.nome}</p>
+                <p>Idade: {e.idade}</p>
+             </div>
+             ))}
+    <div className='card2'>
+      {texto}
+    </div>
+      </div>
       </div>
     </>
   )
