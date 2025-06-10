3a6520ed215c69830badd1bf55a66f199e6e2501
felype carlos <felype.carlosilva@gmail.com>
2025-06-10 15:47:28 -0300

    salvar 10 06 2

--- src/App.jsx
+++ src/App.jsx
@@ -31,19 +31,40 @@ function App() {
     <div><h1>CHECKLIST</h1></div>
     <div className='card1'><input type='text' 
                  placeholder='Título' 
-                  onChange={handleMateria}
-                  value={materia}/>
+                  onChange={handleTitulo}
+                  value={titulo}/>
                   <input type="text"
                   placeholder='Descrição'
-                  onChange={handleDia}
-                  value={dia}/>
+                  onChange={handleDescrição}
+                  value={descrição}/>
+                  <select>
+  <option value="valor1">Matemática</option>
+  <option value="valor2">Português</option>
+  <option value="valor3">Física</option>
+  <option value="valor4">História</option>
+  <option value="valor5">Geografia</option>
+  <option value="valor6">Química</option>
+  <option value="valor7">Religião</option>
+  <option value="valor8">Filosofia</option>
+  onChange={handleMateria}
+</select>
+<select>
+  <option value="valor1">Segunda</option>
+  <option value="valor2">Terça</option>
+  <option value="valor3">Quarta</option>
+  <option value="valor4">Quinta</option>
+  <option value="valor5">Sexta</option>
+  onChange={handleDia}
+</select>
              <button onClick={handleClick}>Adicionar Tarefa </button>
             
     <div className='card2'>
     {tarefas.map(e=>(
               <div key={e.materia}>
-                <p>matéria: {e.materia}</p>
-                <p>dia: {e.dia}</p>
+                <p>Título: {e.titlo}</p>
+                <p>Descrição: {e.descricao}</p>
+                <p>Matéria: {e.materia}</p>
+                <p>Dia: {e.dia}</p>
              </div>
              ))}
     </div>
